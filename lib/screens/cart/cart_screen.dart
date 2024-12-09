35aef1f9caea52e11f9b5b9ae1e16a26dacd353d
Aayush <ayushsakpal007@gmail.com>
2024-12-09 18:47:21 +0530

    First commit

---
+++ lib/screens/cart/cart_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/providers/product_provider.dart';
+
+class CartScreen extends ConsumerStatefulWidget {
+  const CartScreen({super.key});
+
+  @override
+  ConsumerState<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends ConsumerState<CartScreen> {
+  
+  bool showCoupon = true;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final cartProducts = ref.watch(reducedProductsProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Cart'),
+        centerTitle: true,
+        // actions: [],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Column(
+              children: cartProducts.map((product) {
+                return Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Image.asset(product.image, width: 60, height: 60,),
+                      const SizedBox(width: 10,),
+                      Text('${product.title}...'),
+                      const Expanded(child: SizedBox()),
+                      Text('\u{20b9} ${product.price}')
+                    ],
+                  ),
+                );
+              }).toList(), // output cart products here
+            ),
+
+            // output totals here
+          ],
+        ),
+      ),
+    );
+  }
+}
